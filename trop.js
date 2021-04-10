374000a22d813a529aa215a1d3b24b86195269b6
samy belmiloud <faycal.belmiloud@eshra.dz>
2021-04-10 12:34:15 +0200

    first commit

---
+++ trop.js
@@ -0,0 +1,36 @@
+
+    var image = [
+        'im01.jpg',
+        'im02.jpg',
+        'im03.jpg',
+        'im04.jpg',
+        'im05.jpg',
+    ]
+    var cpt =0
+
+function change() {
+var img = document
+.getElementById('img-slider')
+img.setAttribute('src','images/' + image[cpt])
+}
+
+setInterval(() => {
+    change();
+    cpt++;
+    if (cpt>4) cpt = 0;
+    
+}, 6000);
+
+function next() {
+    cpt++;
+    if(cpt > 4 ) cpt=0;
+    change()
+    
+}
+
+function previous() {
+    cpt--;
+    if(cpt < 0 ) cpt=4;
+    change()
+    
+}
